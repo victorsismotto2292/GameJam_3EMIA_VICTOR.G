9c48096850f9bda903a827f3286c46aca5e52d61
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-06-09 22:12:26 -0300

    Ajustando valores
    Valores para tileset, tamanho da colisão e constantes do script do personagem atualizados

--- ac-01-games-2tri/scripts/player.gd
+++ ac-01-games-2tri/scripts/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 # Configurações de Movimento
-const SPEED = 150.0
-const RUN_SPEED = 250.0
+const SPEED = 300
+const RUN_SPEED = 375
 const JUMP_VELOCITY = -400.0
-const DOUBLE_JUMP_VELOCITY = -300.0 # Para pulo-duplo
+const DOUBLE_JUMP_VELOCITY = -450.0 # Para pulo-duplo
 const MAX_JUMPS = 2 # Para pulo-duplo
 
 var current_jumps = 0
