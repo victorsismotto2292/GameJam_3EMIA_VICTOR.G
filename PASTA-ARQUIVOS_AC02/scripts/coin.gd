c74b1c9ba06ef50e447135b74e887255a7ed8ff5
Vecyn_SisMoTTO <167320276+victorsismotto2292@users.noreply.github.com>
2026-06-24 11:43:47 -0300

    Fazendo moeda
    Partícula da moeda bugada (gigante)

---
+++ PASTA-ARQUIVOS_AC02/scripts/coin.gd
@@ -0,0 +1,21 @@
+# CÓDIGO INICIAL ATUALIZADO COM AS PARTÍCULAS MAS SEM O HUD:
+
+extends Area2D
+
+signal collected
+
+# FChamando as variáveis dos nós
+@onready var particles: GPUParticles2D = $Particles
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+# função do parâmetro body_entered:
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		print(+1) # SEM HUD
+		collected.emit()
+		sprite_2d.visible = false
+		collision_shape_2d.set_deferred("disabled", true)
+		particles.emitting = true
+		await particles.finished
+		queue_free()
